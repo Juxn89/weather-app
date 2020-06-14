c8d3d78948d11edf69fdc5f19674ea2f732e2b75
Juan Gómez <gb.jc@outllok.com>
2020-06-14 17:44:41 -0600

    Se crea la base para pasar parámetro de un componente a otro.

--- src/componentes/weatherLocation.js
+++ src/componentes/weatherLocation.js
@@ -2,6 +2,11 @@ import React from 'react';
 import Location from './locations';
 import WeatherData from './weatherData';
 
-const WeatherLocation = () => <div><Location/><WeatherData/></div>;
+const WeatherLocation = () => (
+    <div>
+        <Location city={'Buenos Aires'}/>
+        <WeatherData/>
+    </div>
+);
 
 export default WeatherLocation;
