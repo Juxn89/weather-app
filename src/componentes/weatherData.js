c16a4a3b2dc6ea9e0541f70bab4d3386fd2d7632
Juan Gómez <gb.jc@outllok.com>
2020-06-14 19:43:04 -0600

    Se corrige en el index.html la referencia del css de weathericons, y pasa parámetro entre componentes del weatherData al weatherTemperature y se muestra el icon en base al valor.

--- src/componentes/weatherData.js
+++ src/componentes/weatherData.js
@@ -3,8 +3,8 @@ import WeatherTemperature from './weatherTemperature';
 import WeatherExtrainfo from './weatherExtrainfo';
 
 const WeatherData = () => (
-    <div>
-        <WeatherTemperature/>
+    <div>        
+        <WeatherTemperature temperature={20}/>
         <WeatherExtrainfo humidity={80} wind={'10 m/s'}/>
     </div>);
 
--- src/componentes/weatherTemperature.js
+++ src/componentes/weatherTemperature.js
@@ -1,5 +1,21 @@
 import React from 'react';
+import WeatherIcons from 'react-weathericons';
 
-const WeatherTemperature = () => <div><span>12 C°</span></div>
+const getWeatherIcon = weatherState => {
+    switch(weatherState) {
+        case 'cloud':
+            return (<WeatherIcons name="cloud" size="2x"/>);
+        default:
+            return (<WeatherIcons name="sleet" size="2x"/>);
+    }
+}
+
+const WeatherTemperature = ( {temperature, weatherState} ) => (
+    <div>
+        {getWeatherIcon(weatherState)}
+        {/* <WeatherIcons name="sleet" size="2x" /> */}
+        <span>{`${temperature} C°`}</span>
+    </div>
+);
 
 export default WeatherTemperature
