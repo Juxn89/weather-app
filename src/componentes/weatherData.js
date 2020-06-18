987e2d55268ec621ba793ab6f29811150a051f65
Juan Gómez <gb.jc@outllok.com>
2020-06-18 02:33:29 -0600

    Se refacctoriza la función getWeatherIcons y se implementan constantes para el uso de los estados del clima.

--- src/componentes/weatherData.js
+++ src/componentes/weatherData.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import WeatherTemperature from './weatherTemperature';
 import WeatherExtrainfo from './weatherExtrainfo';
+import { SNOW } from '../constants/weathers';
 
 const WeatherData = () => (
     <div>        
-        <WeatherTemperature temperature={20}/>
+        <WeatherTemperature temperature={20} weatherState={SNOW}/>
         <WeatherExtrainfo humidity={80} wind={'10 m/s'}/>
     </div>);
 
